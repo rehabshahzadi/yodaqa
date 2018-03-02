884f3e7c8bc2c0e12aa18cd7530358663bee3dca
Muhammad Wasim <cmwasim@gmail.com>
2018-03-02 15:09:31 +0500

    added files for YOdaQAmachine for getting input

---
+++ src/main/java/cz/brmlab/yodaqa/io/machine/IOHelper.java
@@ -0,0 +1,59 @@
+package cz.brmlab.yodaqa.io.machine;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class IOHelper {
+	public static ArrayList<InputQuestion> ReadQuestions(String[] paths)
+			throws FileNotFoundException, IOException, ParseException {
+		ArrayList<InputQuestion> questions = new ArrayList<InputQuestion>();
+		JSONParser parser = new JSONParser();
+
+		for (String path : paths) {
+			JSONObject jsonFile = (JSONObject) parser.parse(new FileReader(path));
+			JSONArray jsonQuestions = (JSONArray) jsonFile.get("questions");
+
+			for (int i = 0; i < jsonQuestions.size(); i++) {
+				JSONObject jsonQuestion = (JSONObject) jsonQuestions.get(i);
+				InputQuestion question = new InputQuestion();
+
+				question.id = jsonQuestion.get("id").toString();
+				question.body = jsonQuestion.get("body").toString();
+				question.type = jsonQuestion.get("type").toString();
+
+				questions.add(question);
+			}
+		}
+
+		return questions;
+	}
+
+	public static void writeResults(String system_file, String results) throws IOException {
+		BufferedWriter writer;
+
+		File f = new File(system_file);
+
+		if (f.exists()) {
+			f.delete();
+		}
+
+		writer = new BufferedWriter(new FileWriter(system_file));
+		writer.write(results);
+
+		writer.flush();
+		writer.close();
+
+		System.out.println("Done!");
+	}
+}
---
+++ src/main/java/cz/brmlab/yodaqa/io/machine/InputQuestion.java
@@ -0,0 +1,7 @@
+package cz.brmlab.yodaqa.io.machine;
+
+public class InputQuestion {
+	public String id;
+	public String body;
+	public String type;
+}
